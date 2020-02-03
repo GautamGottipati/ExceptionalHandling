fd91d0781bf493fc3224eb81575fe911f495c7b9
GautamGottipati <gautamrace@gmail.com>
2020-02-03 13:22:08 +0530

    Adding files :Topic: Exceptional Handling

---
+++ CustomException/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    /// <summary>
+    /// Custom Exception handling
+    /// The denomination should not be zero exception
+    /// </summary>
+    class MyException : ApplicationException
+    {
+        public void MyExceptiona()
+        {
+            Console.WriteLine("An exception occured");
+        }
+        public void MyDivideException()
+        {
+            Console.WriteLine("Exception occured, divisor should not be zero");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d, div, res;
+            div = Int32.Parse(Console.ReadLine());
+            d = Int32.Parse(Console.ReadLine());
+            #region TRY BLOCK
+            try
+            {
+                if (div == 0)
+                {
+                    #region THROW
+                    throw new MyException();
+                    #endregion
+                }
+                else
+                {
+                    res = d / div;
+                    Console.WriteLine("Result : {0}", res);
+                }
+            }
+            #endregion
+            #region CATCH BLOCK
+            catch (MyException e)
+            {
+                e.MyDivideException();
+            }
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExceptionalHandling/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionalHandling
+{
+    class Program
+    {
+        /// <summary>
+        /// This program contains the basic implementations of Exception handling for calculator app
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            double Operand1, Operand2;
+            double Result = 0.00;
+            char Operator;
+            Console.WriteLine("This program allows you to perform an operation on two numbers");
+            #region TRY BLOCK
+            try
+            {
+                Console.Write("Enter a number: ");
+                Operand1 = double.Parse(Console.ReadLine());
+                Console.Write("Enter an operator: ");
+                Operator = char.Parse(Console.ReadLine());
+                Console.Write("Enter a number: ");
+                Operand2 = double.Parse(Console.ReadLine());
+                if (Operator != '+' &&
+                    Operator != '-' &&
+                    Operator != '*' &&
+                    Operator != '/')
+                    throw new Exception(Operator.ToString());
+                if (Operator == '/') if (Operand2 == 0)
+                        throw new DivideByZeroException("Division by zero is not allowed");
+
+                switch (Operator)
+                {
+                    case '+':
+                        Result = Operand1 + Operand2;
+                        break;
+                    case '-':
+                        Result = Operand1 - Operand2;
+                        break;
+                    case '*':
+                        Result = Operand1 * Operand2;
+                        break;
+                    case '/':
+                        Result = Operand1 / Operand2;
+                        break;
+                    default:
+                        Console.WriteLine("Bad Operation");
+                        break;
+                }
+                Console.WriteLine("\n{0} {1} {2} = {3}", Operand1, Operator, Operand2, Result);
+            }
+            #endregion
+            #region CATCH BLOCK
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nOperation Error: {0} is not a valid operator", ex.StackTrace);
+            }
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
